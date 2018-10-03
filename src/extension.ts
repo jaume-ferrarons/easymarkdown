56f884c52d42e373d0d7ebf2e2ef7a8c6c928f2c
Jaume <jaume.ferrarons@smartmonkey.io>
2018-10-03 22:10:13 +0200

    Basic markdown operations

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,44 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function toBold(editor: vscode.TextEditor, selection: vscode.Selection): void {
+    const text = editor.document.getText(selection);
+    editor.edit((edit) => {
+        edit.replace(selection, `**${text}**`);
+    });
+}
+
+function toItalics(editor: vscode.TextEditor, selection: vscode.Selection): void {
+    const text = editor.document.getText(selection);
+    editor.edit((edit) => {
+        edit.replace(selection, `*${text}*`);
+    });
+}
+
+function toStrikethrough(editor: vscode.TextEditor, selection: vscode.Selection): void {
+    const text = editor.document.getText(selection);
+    editor.edit((edit) => {
+        edit.replace(selection, `~~${text}~~`);
+    });
+}
+
+function toLink(editor: vscode.TextEditor, selection: vscode.Selection): void {
+    const text = editor.document.getText(selection);
+    editor.edit((edit) => {
+        edit.replace(selection, `[${text}](https://url.here)`);
+    });
+}
+
+function toListItem(editor: vscode.TextEditor, selection: vscode.Selection): void {
+    const startLine = editor.selection.start.line;
+    const endLine = editor.selection.end.line;
+    editor.edit((edit) => {
+        for (let line = startLine; line <= endLine; line++) {
+            edit.insert(new vscode.Position(line, 0), '* ');
+        }
+    });
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -22,6 +60,43 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+
+    const commands = [
+        { name: 'bold', handler: toBold },
+        { name: 'italics', handler: toItalics },
+        { name: 'strikethrough', handler: toStrikethrough},
+        { name: 'link', handler: toLink },
+        { name: 'item', handler: toListItem }
+    ];
+
+    commands.forEach(command => {
+        context.subscriptions.push(vscode.commands.registerCommand(`extension.${command.name}`, () => {
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return; // No open text editor
+            }
+            let selection = editor.selection;
+            command.handler(editor, selection);
+        }));
+    });
+
+    // context.subscriptions.push(vscode.commands.registerCommand('extension.bold', () => {
+    //     // The code you place here will be executed every time your command is executed
+
+    //     // Display a message box to the user
+    //     vscode.window.showInformationMessage('Hello World!');
+
+    //     let editor = vscode.window.activeTextEditor;
+    //     if (!editor) {
+    //         return; // No open text editor
+    //     }
+
+    //     let selection = editor.selection;
+    //     let text = editor.document.getText(selection);
+    //     editor.edit((edit) => {
+    //         edit.replace(selection, `**${text}**`)
+    //     });
+    // }));
 }
 
 // this method is called when your extension is deactivated
